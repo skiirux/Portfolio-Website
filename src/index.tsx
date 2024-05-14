750ebb4d44b811e08a45d05f87013a692e7c2b40
skiirux <mohitkumar637@gmail.com>
2024-05-14 13:08:15 +0530

    Added the analytics Functionality

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Analytics />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
